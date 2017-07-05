793fcb75bb2be1a32345333295044121cf444fc7
Saulo Aguiar <sauloaguiar89@gmail.com>
2017-07-05 16:32:58 -0300

    Display buy button and redirects to Amazon

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native'; 
+import { Text, View, Image, Linking } from 'react-native'; 
 import { Card } from './Card';
 import { CardSection } from './CardSection';
+import { Button } from './Button';
 
 const styles = {
   headerContentStyle: {
@@ -29,7 +30,7 @@ const styles = {
 };
 
 export const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   return (
     <Card>
       <CardSection>
@@ -49,6 +50,11 @@ export const AlbumDetail = ({ album }) => {
           source={{ uri: image }}
           style={styles.imageStyle}
         />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy
+        </Button>
       </CardSection>
     </Card>
   );
