7eae6fe63b98a49942c18c72b9a86e3fa512cc0b
Saulo Aguiar <sauloaguiar89@gmail.com>
2017-07-05 15:48:03 -0300

    Added images and made the list scrollable

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native'; 
+import { Text, View, Image } from 'react-native'; 
 import { Card } from './Card';
 import { CardSection } from './CardSection';
 
-export const AlbumDetail = (props) => {
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextTitleStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    width: null,
+    flex: 1
+  }
+};
+
+export const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextTitleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={styles.imageStyle}
+        />
       </CardSection>
     </Card>
   );
