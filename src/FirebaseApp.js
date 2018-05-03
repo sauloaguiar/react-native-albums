1cce83e83c371472292ff0ac85bb1ac494a73b8d
Saulo Aguiar <sauloaguiar89@gmail.com>
2018-05-03 16:46:10 -0300

    Revert App entry point to display albums
    upgrades react-native project

---
+++ src/FirebaseApp.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Spinner, Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
+
+export class FirebaseApp extends React.Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDra4XmImEgZTVijP3J9GkuNc2s1liSiMI',
+      authDomain: 'albums-22316.firebaseapp.com',
+      databaseURL: 'https://albums-22316.firebaseio.com',
+      projectId: 'albums-22316',
+      storageBucket: 'albums-22316.appspot.com',
+      messagingSenderId: '145925540170'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Header label={'Albums'} />
+        <View style={{ marginTop: 5, height: 45 }} > 
+          {this.renderContent()}
+        </View>
+        
+      </View>
+    );
+  }
+} 
